cce1fc12feabd9bce9e433bdb6a4b699b268607b
Abanoub Maged <abanoubmaged4598@gmail.com>
2020-09-17 17:58:30 +0200

    xylophone-App
    A Simple Musical Instrument
    App

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+
+  AudioCache player = AudioCache();
+
+  void playSound(int soundNumber) {
+    player.play('note$soundNumber.wav');
+  }
+
+  Widget buildKey(int KeyNumber, Color keyColor){
+    return Expanded(
+      child: FlatButton(
+        onPressed: (){playSound(KeyNumber);},
+        color: keyColor,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
